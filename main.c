2044cdabceaa6179c9ccfdf65ce1672fd81cc4b6
Tolu <olabiyitolu@gmail.com>
2023-11-14 10:58:48 +0100

    exit shell

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
-# include "main.h"
+#include "main.h"
 
 int main(int tc, char **argv){
     char *prompt = "(Tshell) $ ";
     char *lineptr;
     size_t t = 0;
+    ssize_t read_nchars;
 
     /* declaring void variables for the tc and argv */
     (void)tc; (void)argv;
@@ -11,13 +12,18 @@ int main(int tc, char **argv){
     /* creating an infinite loop */
     while (1){
         printf("%s", prompt);
-        getline(&lineptr, &t, stdin);
+        read_nchars = getline(&lineptr, &t, stdin);
+        /* check if user use ctr + c or the function failed or reached EOF */
+            if (read_nchars == -1){
+                printf("Existing tshell......\n");
+                return (-1);
+        }
+
         printf("%s\n", lineptr);
 
         /* code to free memory*/
         free(lineptr);
     }
-
     
     return (0);
 }
